da246757657b40123ed549a5843c08eb7c06411b
ksegun <ksegun@users.noreply.github.com>
2016-09-09 08:47:07 -0500

    update docker installation

--- equip_docker.sh
+++ equip_docker.sh
@@ -7,7 +7,10 @@
 # See http://docs.docker.io/en/latest/installation/ubuntulinux/ 
 #  & https://stackoverflow.com/questions/19412911/docker-installation-on-ubuntu-raring/19416387#19416387
 
-sudo sh -c "curl https://get.docker.io/gpg | apt-key add -"
-sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo sh -c "echo deb https://apt.dockerproject.org/repo ubuntu-trusty main > /etc/apt/sources.list.d/docker.list"
+sudo apt-get purge lxc-docker
+sudo apt-cache policy docker-engine
+sudo apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
 sudo apt-get update
-sudo apt-get install lxc-docker -y
+sudo apt-get install -y docker-engine
