ee1890b537d53a9a4dfc765bf1e9657eba38eca5
Kolawole Segun <segunk@us.ibm.com>
2016-09-11 14:05:01 -0500

    missing bash

--- equip_docker_compose.sh
+++ equip_docker_compose.sh
@@ -6,7 +6,7 @@
 # Licence: MIT
 # See http://docs.docker.io/en/latest/installation/ubuntulinux/
 #  & https://stackoverflow.com/questions/19412911/docker-installation-on-ubuntu-raring/19416387#19416387
-wget --no-check-certificate -qO- https://github.com/ksegun/ubuntu-equip/raw/master/equip_docker.sh | sudo -
+wget --no-check-certificate -qO- https://github.com/ksegun/ubuntu-equip/raw/master/equip_docker.sh | sudo bash -
 
 curl -OL https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > docker-compose
 sudo mv docker-compose /usr/local/bin/docker-compose
