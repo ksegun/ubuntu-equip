32dac9c1416cc30fdf0bc81ecefa0748afb3ec40
Kolawole Segun <segunk@us.ibm.com>
2016-09-11 14:14:57 -0500

    fix curl option

--- equip_docker_compose.sh
+++ equip_docker_compose.sh
@@ -8,6 +8,6 @@
 #  & https://stackoverflow.com/questions/19412911/docker-installation-on-ubuntu-raring/19416387#19416387
 wget --no-check-certificate -qO- https://github.com/ksegun/ubuntu-equip/raw/master/equip_docker.sh | sudo bash -
 
-curl -OL https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > docker-compose
 sudo mv docker-compose /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
