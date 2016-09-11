c054527aa850e2c57fd64c75cb4ab32668ea7d24
Kolawole Segun <segunk@us.ibm.com>
2016-09-11 13:03:18 -0500

    update base

--- equip_base.sh
+++ equip_base.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #
-# Ubuntu Equip 
+# Ubuntu Equip
 #  Equip basec/core libs
 # Licence: MIT
 
@@ -13,3 +13,5 @@ sudo apt-get install libssl-dev zlib1g-dev libcurl3-dev libxslt-dev -y
 sudo apt-get install software-properties-common python-software-properties -y
 
 sudo apt-get install git -y
+
+sudo apt-get install apt-transport-https ca-certificates -y
